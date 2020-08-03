e5de28b2ebf7ac7ae3f1a16cc8144c522eeff628
YiniXu9506 <xuyini@pingcap.com>
2020-08-03 12:18:24 +0800

    chore: update download handle sync

--- markdown-pages/download.js
+++ markdown-pages/download.js
@@ -76,41 +76,60 @@ async function retrieveAllMDs(metaInfo, distDir, pipelines = []) {
   })
 }
 
-async function retrieveAllMDs(metaInfo, distDir, pipelines = []) {
-  const { owner, repo, ref, path } = metaInfo
+async function handleSync(metaInfo, pipelines = []) {
+  const { owner, repo, ref, base, head } = metaInfo
+  if (base && head) {
+    const { files } = (await getCommitInfo(owner, repo, base, head)).data
 
-  const list = (await getContents(owner, repo, ref, path)).data
+    files.forEach((file) => {
+      const { filename, status, raw_url } = file
 
-  list.forEach((el) => {
-    const { name, type, download_url } = el
-
-    if (shouldIgnorePath(name)) {
-      return
-    }
-
-    if (type === 'dir') {
-      const nextDistDir = `${distDir}/${name}`
-
-      if (!fs.existsSync(nextDistDir)) {
-        fs.mkdirSync(nextDistDir)
+      if (shouldIgnorePath(filename)) {
+        return
       }
 
-      retrieveAllMDs(
-        {
-          owner,
-          repo,
-          ref,
-          path: `${path}/${name}`,
-        },
-        `${distDir}/${name}`,
-        pipelines
-      )
-    } else {
-      if (name.endsWith('.md')) {
-        writeContent(download_url, `${distDir}/${name}`, pipelines)
+      let path
+      if (repo === 'docs-tidb-operator' || repo === 'docs-dm') {
+        const basePath = filename.split('/').slice(1).join('/')
+
+        if (filename.startsWith('en')) {
+          path = `${__dirname}/contents/en/${repo}/${ref}/${basePath}`
+        } else if (filename.startsWith('zh')) {
+          path = `${__dirname}/contents/zh/${repo}/${ref}/${basePath}`
+        } else {
+          return
+        }
+      } else if (repo === 'dbaas-docs') {
+        path = `${__dirname}/contents/en/docs-dbaas/${ref}/${filename}`
+      } else if (repo === 'docs') {
+        path = `${__dirname}/contents/en/docs-tidb/${ref}/${filename}`
+      } else if (repo === 'docs-cn') {
+        path = `${__dirname}/contents/zh/docs-tidb/${ref}/${filename}`
+      } else {
+        return
       }
-    }
-  })
+
+      switch (status) {
+        case 'added':
+        case 'modified':
+          writeContent(raw_url, path, pipelines)
+
+          break
+        case 'removed':
+          fs.unlink(path, (err) => {
+            if (err) {
+              sig.error(`Fail to unlink ${path}: ${err}`)
+            } else {
+              sig.success(`Deleted: ${path}`)
+            }
+          })
+
+          break
+        default:
+          break
+      }
+    })
+  }
 }
 
 module.exports = {
